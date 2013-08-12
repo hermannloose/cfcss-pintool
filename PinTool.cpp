63d95bec5174aa39bb67274cce63fe0b5265070e
Hermann Loose <hermannloose@gmail.com>
2013-08-12 15:39:52 +0200

    Iterate over routines containing requested addresses, without instrumentation.

--- PinTool.cpp
+++ PinTool.cpp
@@ -11,14 +11,22 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <list>
 #include <set>
+
+
+std::ostream * out = &cerr;
 
 static string red = "\x1b[31;1m";
 static string green = "\x1b[32;1m";
 static string reset = "\x1b[0m";
 
-set<ADDRINT> *addressesToInstrument;
-set<ADDRINT> *addressesToIgnore;
+set<ADDRINT> *addressesToInstrument = new set<ADDRINT>();
+set<ADDRINT> *addressesToIgnore = new set<ADDRINT>();
+
+
+list<pair<ADDRINT, ADDRINT>> *segments;
+
 
 
 KNOB<string> KnobAddressFiles(
@@ -54,9 +62,14 @@ BOOL HandleSignal(
   switch (signal) {
     case 4:
       cerr << "CFCSS detected a control-flow violation! (SIGILL)" << endl;
+
+      *out << "--- SIGILL ---" << endl;
+
       break;
     default:
       cerr << "Signal: " << signal << endl;
+
+      *out << "--- Signal: " << signal << " ---" << endl;
   }
 
   cerr << "EIP:    " << showbase << hex << PIN_GetContextReg(context, REG_EIP)
@@ -66,6 +79,56 @@ BOOL HandleSignal(
   return TRUE;
 }
 
+VOID HandleRoutine(RTN routine) {
+  ADDRINT routineStart = RTN_Address(routine);
+  ADDRINT routineEnd = routineStart + RTN_Size(routine);
+
+  auto ai = addressesToInstrument->lower_bound(routineStart);
+  auto ae = addressesToInstrument->upper_bound(routineEnd);
+  auto end = addressesToInstrument->end();
+
+  if (ai != ae && ai != end) {
+    cerr << "[" << RTN_Name(routine) << "]" << endl;
+    RTN_Open(routine);
+    for (INS instruction = RTN_InsHead(routine); INS_Valid(instruction), ai != ae;
+        instruction = INS_Next(instruction)) {
+
+      ADDRINT address = INS_Address(instruction);
+      if (address == *ai) {
+        ++ai;
+      }
+    }
+    RTN_Close(routine);
+  }
+}
+
+VOID HandleExecutableSection(SEC section) {
+  ADDRINT sectionStart = SEC_Address(section);
+  ADDRINT sectionEnd = sectionStart + SEC_Size(section);
+
+  segments->push_back(pair<ADDRINT, ADDRINT>(sectionStart, sectionEnd));
+
+  auto ai = addressesToInstrument->lower_bound(sectionStart);
+  auto ae = addressesToInstrument->upper_bound(sectionEnd);
+  auto end = addressesToInstrument->end();
+
+  if (ai != ae && ai != end) {
+    cerr << "[" << SEC_Name(section) << "]" << endl;
+    for (RTN routine = SEC_RtnHead(section); RTN_Valid(routine); routine = RTN_Next(routine)) {
+      HandleRoutine(routine);
+    }
+  }
+}
+
+VOID ImageLoad(IMG image, VOID *v) {
+  cerr << "Loading image [" << IMG_Name(image) << "] ..." << endl;
+
+  for (SEC section = IMG_SecHead(image); SEC_Valid(section); section = SEC_Next(section)) {
+    if (SEC_IsExecutable(section)) {
+      HandleExecutableSection(section);
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   // Initialize PIN library. Print help message if -h(elp) is specified
