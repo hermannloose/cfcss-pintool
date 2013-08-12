5456c48fc6878775c6c165f56adb33718661f7b4
Hermann Loose <hermannloose@gmail.com>
2013-08-12 13:32:00 +0200

    Pass address whitelists/blacklists on the command line.

--- PinTool.cpp
+++ PinTool.cpp
@@ -6,7 +6,37 @@
 
 #include "pin.H"
 
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <set>
+
+static string red = "\x1b[31;1m";
+static string green = "\x1b[32;1m";
+static string reset = "\x1b[0m";
+
+set<ADDRINT> *addressesToInstrument;
+set<ADDRINT> *addressesToIgnore;
+
+
+KNOB<string> KnobAddressFiles(
+    KNOB_MODE_APPEND,
+    "pintool",
+    "address-file",
+    "",
+    "file containing a list of addresses to instrument at, one per line\n"
+    "(can be specified multiple times)");
+
+KNOB<string> KnobIgnoreFiles(
+    KNOB_MODE_APPEND,
+    "pintool",
+    "ignore-file",
+    "",
+    "file containing a list of addresses to ignore, one per line\n"
+    "(can be specified multiple times)");
+
 INT32 Usage() {
   cerr << KNOB_BASE::StringKnobSummary() << endl;
 
@@ -52,6 +82,68 @@ int main(int argc, char *argv[]) {
 
   // Intercept SIGSEGV, since these will count as "detected" errors as well.
   PIN_InterceptSignal(11, HandleSignal, 0);
+
+  addressesToInstrument = new set<ADDRINT>();
+
+  int addressFilesCount = KnobAddressFiles.NumberOfValues();
+  for (int i = 0; i < addressFilesCount; ++i) {
+    string addressFileName = KnobAddressFiles.Value(i);
+    if (!addressFileName.empty()) {
+      ifstream addressFile(addressFileName);
+      if (addressFile.is_open()) {
+        ADDRINT address;
+        addressFile >> hex;
+        while (addressFile >> address) {
+          addressesToInstrument->insert(address);
+        }
+      } else {
+        cerr << "Could not open " << addressFileName << " for reading!" << endl;
+
+        return -1;
+      }
+    }
+  }
+
+  addressesToIgnore = new set<ADDRINT>();
+
+  int ignoreFilesCount = KnobIgnoreFiles.NumberOfValues();
+  for (int i = 0; i < ignoreFilesCount; ++i) {
+    string ignoreFileName = KnobIgnoreFiles.Value(i);
+    if (!ignoreFileName.empty()) {
+      ifstream ignoreFile(ignoreFileName);
+      if (ignoreFile.is_open()) {
+        ADDRINT address;
+        ignoreFile >> hex;
+        while (ignoreFile >> address) {
+          addressesToIgnore->insert(address);
+        }
+      } else {
+        cerr << "Could not open " << ignoreFileName << " for reading!" << endl;
+
+        return -1;
+      }
+    }
+  }
+
+  set<ADDRINT> intersection;
+  set_intersection(
+      addressesToInstrument->begin(), addressesToInstrument->end(),
+      addressesToIgnore->begin(), addressesToIgnore->end(),
+      inserter(intersection, intersection.begin()));
+
+  if (!intersection.empty()) {
+    cerr << red << "The following addresses were requested to both be instrumented and ignored:"
+        << reset << endl;
+
+    cerr << showbase << hex;
+    for (auto i = intersection.begin(), e = intersection.end(); i != e; ++i) {
+      cerr << *i << endl;
+    }
+    cerr << noshowbase << dec;
+
+    return -1;
+  }
+
 
   // Start the program, never returns.
   PIN_StartProgram();
