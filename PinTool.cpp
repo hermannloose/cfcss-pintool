b2119331cc7e48a6e56f63df46ee62d66402ddb6
Hermann Loose <hermannloose@gmail.com>
2013-08-13 13:21:01 +0200

    Add capability to delete instructions and insert branches.

--- PinTool.cpp
+++ PinTool.cpp
@@ -9,13 +9,18 @@
 #include <algorithm>
 #include <cstdlib>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <list>
+#include <random>
 #include <set>
 
 
 std::ostream * out = &cerr;
+
+default_random_engine *generator;
+uniform_int_distribution<unsigned int> *distribution;
 
 static string red = "\x1b[31;1m";
 static string green = "\x1b[32;1m";
@@ -24,10 +29,31 @@ static string reset = "\x1b[0m";
 set<ADDRINT> *addressesToInstrument = new set<ADDRINT>();
 set<ADDRINT> *addressesToIgnore = new set<ADDRINT>();
 
+enum INSTRUMENT_ACTION {
+  NONE,
+  DELETE,
+  INSERT_BRANCH,
+  BRANCH_GARBLE
+};
+
+INSTRUMENT_ACTION instrumentAction = NONE;
 
 list<pair<ADDRINT, ADDRINT>> *segments;
 
+KNOB<unsigned int> KnobRandomSeed(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "seed",
+    "0",
+    "PRNG seed, use to reproduce runs of the tool that involved random bit flips."
+    "A given seed of zero is ignored and std::random_device is used instead.");
 
+KNOB<string> KnobOutputFile(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "o",
+    "",
+    "write output to file");
 
 KNOB<string> KnobAddressFiles(
     KNOB_MODE_APPEND,
@@ -44,6 +70,20 @@ KNOB<string> KnobIgnoreFiles(
     "",
     "file containing a list of addresses to ignore, one per line\n"
     "(can be specified multiple times)");
+
+KNOB<string> KnobInstrumentAction(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "instrument-action",
+    "delete",
+    "one of (delete|insert-branch|branch-garble)");
+
+KNOB<BOOL> KnobLimitJumpsToExecutableSections(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "avoid-segfaults",
+    "1",
+    "whether to limit inserted or modified branches to targets within executable sections");
 
 INT32 Usage() {
   cerr << KNOB_BASE::StringKnobSummary() << endl;
@@ -66,17 +106,78 @@ BOOL HandleSignal(
       *out << "--- SIGILL ---" << endl;
 
       break;
+    case 11:
+      *out << "--- SIGSEGV ---" << endl;
+
+      break;
     default:
       cerr << "Signal: " << signal << endl;
 
       *out << "--- Signal: " << signal << " ---" << endl;
   }
 
-  cerr << "EIP:    " << showbase << hex << PIN_GetContextReg(context, REG_EIP)
-      << noshowbase << dec << endl;
+  ADDRINT eip = PIN_GetContextReg(context, REG_EIP);
+  cerr << "EIP:    " << showbase << hex << eip << noshowbase << dec << endl;
+  if (none_of(segments->begin(), segments->end(),
+      [&] (pair<ADDRINT, ADDRINT> segment) {
+        return segment.first <= eip && segment.second >= eip;
+      })) {
+
+    cerr << "(not within an executable section)" << endl;
+  }
+
 
   // Pass the signal on to the application.
   return TRUE;
+}
+
+ADDRINT FlipBitInAddress(ADDRINT address, BOOL limitToExecutableSegments) {
+  ADDRINT target;
+
+  do {
+    unsigned int position = (*distribution)(*generator);
+    target = address ^ (1 << position);
+  } while (limitToExecutableSegments && none_of(segments->begin(), segments->end(),
+        [&] (pair<ADDRINT, ADDRINT> range) {
+          return range.first <= target && range.second >= target;
+        }));
+
+  return target;
+}
+
+VOID InstrumentInstruction(INS instruction) {
+  ADDRINT address = INS_Address(instruction);
+
+  cerr << showbase << hex << address << noshowbase << dec << ": ";
+
+  switch (instrumentAction) {
+    case DELETE:
+      {
+        int width = cerr.width();
+        cerr.width(8);
+        cerr << "deleting \"" << INS_Disassemble(instruction) << "\"" << endl;
+        cerr.width(width);
+
+        *out << "del " << showbase << hex << address << noshowbase << dec << endl;
+
+        INS_Delete(instruction);
+      }
+      break;
+    case INSERT_BRANCH:
+      {
+        ADDRINT target = FlipBitInAddress(address, KnobLimitJumpsToExecutableSections.Value());
+
+        cerr << "inserting direct jump to "
+            << showbase << hex << target << noshowbase << dec << endl;
+
+        *out << "ins " << showbase << hex << address << " " << target << noshowbase << dec << endl;
+
+        INS_InsertDirectJump(instruction, IPOINT_BEFORE, target);
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 VOID HandleRoutine(RTN routine) {
@@ -95,6 +196,7 @@ VOID HandleRoutine(RTN routine) {
 
       ADDRINT address = INS_Address(instruction);
       if (address == *ai) {
+        InstrumentInstruction(instruction);
         ++ai;
       }
     }
@@ -139,14 +241,27 @@ int main(int argc, char *argv[]) {
 
   PIN_InitSymbols();
 
+  string fileName = KnobOutputFile.Value();
+  if (!fileName.empty()) {
+    out = new std::ofstream(fileName.c_str());
+  }
+
+  unsigned int seed = KnobRandomSeed.Value();
+  if (!seed) {
+    seed = random_device{}();
+  }
+  cerr << "Random seed: " << seed << endl;
+  *out << "Random seed: " << seed << endl;
+
+  generator = new default_random_engine(seed);
+  distribution = new uniform_int_distribution<unsigned int>(0, 63);
+
   // Intercept SIGILL, raised by UD2 instruction in CFCSS, signalling
   // a signature mismatch.
   PIN_InterceptSignal(4, HandleSignal, 0);
 
   // Intercept SIGSEGV, since these will count as "detected" errors as well.
   PIN_InterceptSignal(11, HandleSignal, 0);
-
-  addressesToInstrument = new set<ADDRINT>();
 
   int addressFilesCount = KnobAddressFiles.NumberOfValues();
   for (int i = 0; i < addressFilesCount; ++i) {
@@ -166,8 +281,6 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-
-  addressesToIgnore = new set<ADDRINT>();
 
   int ignoreFilesCount = KnobIgnoreFiles.NumberOfValues();
   for (int i = 0; i < ignoreFilesCount; ++i) {
@@ -207,6 +320,25 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  string instrumentActionFlag = KnobInstrumentAction.Value();
+  if (instrumentActionFlag == "delete") {
+    instrumentAction = DELETE;
+  }
+  if (instrumentActionFlag == "insert-branch") {
+    instrumentAction = INSERT_BRANCH;
+  }
+
+  segments = new list<pair<ADDRINT, ADDRINT>>();
+
+  IMG_AddInstrumentFunction(ImageLoad, 0);
+
+  cerr <<  "=================================================" << endl;
+  cerr <<  "This application is instrumented by cfcss-pintool" << endl << endl;
+  if (!fileName.empty()) {
+      cerr << "See file " << endl << fileName << endl
+          << "for a log of the modifications performed." << endl;
+  }
+  cerr <<  "=================================================" << endl;
 
   // Start the program, never returns.
   PIN_StartProgram();
